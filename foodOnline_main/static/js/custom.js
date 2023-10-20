f53dd6e5071eb6d1d8f462ce2430ec3ee8d2f56b
vuphucpy <vuphucpy47@gmail.com>
2023-10-20 16:59:39 +0700

    dynamic tax module

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
                     // subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amounts['subtotal'],
-                        response.cart_amounts['tax'],
+                        response.cart_amounts['tax_dict'],
                         response.cart_amounts['grand_total'],
                     )
                 }
@@ -82,7 +82,7 @@ $(document).ready(function(){
                     // subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amounts['subtotal'],
-                        response.cart_amounts['tax'],
+                        response.cart_amounts['tax_dict'],
                         response.cart_amounts['grand_total'],
                     )
                 }
@@ -116,7 +116,7 @@ $(document).ready(function(){
                     // subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amounts['subtotal'],
-                        response.cart_amounts['tax'],
+                        response.cart_amounts['tax_dict'],
                         response.cart_amounts['grand_total'],
                     )
                 }
@@ -142,11 +142,16 @@ $(document).ready(function(){
     }
 
     // apply cart amounts
-    function applyCartAmounts(subtotal, tax, grand_total){
+    function applyCartAmounts(subtotal, tax_dict, grand_total){
         if (window.location.pathname == '/cart/'){
             $('#subtotal').html(subtotal)
-            $('#tax').html(tax)
             $('#total').html(grand_total)
+
+            for (key1 in tax_dict){
+                for (key2 in tax_dict[key1]){
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+                }
+            }
         }
     }
 
--- marketplace/context_processors.py
+++ marketplace/context_processors.py
@@ -1,4 +1,4 @@
-from .models import Cart
+from .models import Cart, Tax
 
 from menu.models import FoodItem
 
@@ -32,6 +32,7 @@ def get_cart_amounts(request):
     tax = 0
     total = 0
     grand_total = 0
+    tax_dict = {}
 
     if request.user.is_authenticated:
         # get cart items
@@ -44,6 +45,20 @@ def get_cart_amounts(request):
             # calc subtotal
             subtotal += (food_item.price * item.quantity)
 
+        # get tax
+        get_tax = Tax.objects.filter(is_active=True)
+
+        # for loop
+        for i in get_tax:
+            tax_type = i.tax_type
+            tax_percentage = i.tax_percentage
+            tax_amount = round((tax_percentage*subtotal)/100, 2)
+            tax_dict.update({tax_type: {str(tax_percentage): tax_amount}})
+
+        tax = sum(x for key in tax_dict.values() for x in key.values())
+
+        print(tax)
+
         grand_total = subtotal + tax
 
-    return dict(subtotal=subtotal, tax=tax, grand_total=grand_total)
+    return dict(subtotal=subtotal, tax=tax, grand_total=grand_total, tax_dict=tax_dict)
