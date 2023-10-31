d50e55ab9c020d02bc6c324500c743b740b132c7
vuphucpy <vuphucpy47@gmail.com>
2023-10-31 19:03:26 +0700

    added cname

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'django-foodonline-env.eba-zfi9krpv.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
