2fd4822813716a7538f75d010c6a88fa6cdbe57e
vuphucpy <vuphucpy47@gmail.com>
2023-11-02 22:43:12 +0700

    remove cname

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-    'django-foodOnline-env.eba-dqwyzru2.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -155,7 +154,7 @@ MEDIA_ROOT = BASE_DIR / MEDIA_URL
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Messages Tag Config
 MESSAGE_TAGS = {
