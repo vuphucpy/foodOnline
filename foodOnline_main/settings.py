cf3b5412a9ed9f92c4531ae6dff264a8eacf2040
vuphucpy <vuphucpy47@gmail.com>
2023-11-05 00:58:22 +0700

    deploying to s3

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
-    'django-FoodOnline-env.eba-93mspyey.us-west-2.elasticbeanstalk.com']
+    'django-FoodOnline-env.eba-93mspyey.us-west-2.elasticbeanstalk.com', "*"]
 
 
 # Application definition
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'customers',
     'orders',
     'admin_honeypot',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -152,11 +153,31 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / STATIC_URL
+# STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / STATIC_URL
+# STATICFILES_DIRS = [
+#     'foodOnline_main/static'
+# ]
+
+# AWS S3 Static Files Configuration
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
+
 STATICFILES_DIRS = [
-    'foodOnline_main/static'
+    'foodOnline_main/static',
 ]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'foodOnline_main.media_storages.MediaStorage'
 
 # Media Files Config
 MEDIA_URL = 'media/'
